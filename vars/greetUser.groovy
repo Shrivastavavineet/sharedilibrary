8b421dd28fb9c40286669cf5124c6783feecc4d4
Shrivastavavineet <vshrivastava902@gmail.com>
2026-05-17 10:51:25 +0000

    Updated greetUser to accept name parameter

--- vars/greetUser.groovy
+++ vars/greetUser.groovy
@@ -1,3 +1,7 @@
-def call() {
-    echo "Hello! Shared Library is working."
+// Purana code shayad sirf 'def call()' tha, use aise karein:
+def call(String name) {
+    echo "Hello ${name}, welcome to the pipeline!"
+    // Agar aap resource file read kar rahe hain toh:
+    def fileContent = libraryResource 'index.html'
+    echo "Resource Content: ${fileContent}"
 }
