86c5cf96606a2f427df2a7818546da36508e8849
Shrivastavavineet <vshrivastava902@gmail.com>
2026-05-17 11:38:28 +0000

    updated gitPush function

--- vars/gitPush.groovy
+++ vars/gitPush.groovy
@@ -1,4 +1,18 @@
-def call(String branchName) {
-    echo "Pushing code to branch: ${branchName}"
-    sh "git push origin ${branchName}"
+def call(branchName) {
+
+    withCredentials([usernamePassword(
+        credentialsId: 'github-creds',
+        usernameVariable: 'GIT_USERNAME',
+        passwordVariable: 'GIT_PASSWORD'
+    )]) {
+
+        sh """
+        git config user.name "Shrivastavavineet"
+        git config user.email "vshrivastava902@gmail.com"
+
+        git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Shrivastavavineet/sharedilibrary.git
+
+        git push origin ${branchName}
+        """
+    }
 }
